1b4b4b86575ec98fc0c47a680fa889fb72c0783b
Samuel Lucas <samuellucas0603@gmail.com>
2020-05-04 15:14:04 -0300

    Implementando letra b da questão numero 1.

--- exercicios/1-b.r
+++ exercicios/1-b.r
@@ -1,4 +1,3 @@
-
 install.packages("data.table")
 install.packages("magrittr")
 install.packages("corrplot")
@@ -11,17 +10,132 @@ library(magrittr)
 library(dplyr)
 library(plyr) 
 
-df_total_alta <- read.csv(file = './src/alta-regiao-2-alta - Página1.csv')
-df_total_baixa <- read.csv(file = './src/alta-regiao-2-baixa - Página1.csv')
+df_total_alta = read.csv(file = './src/alta-regiao-2-alta - Página1.csv')
+df_total_baixa = read.csv(file = './src/alta-regiao-2-baixa - Página1.csv')
 
 # Utils
 # =================================
 # apply(dadosExemplo, MARGIN = 2, FUN = mean)
 # colnames(df_total_alta)
-# str(df_aux)
-# warnings()
 
-# ====================================================================================
-# - b) Em quais meses a doença_café foi mais incidente? Houve variação por ano? 
-# (ex: a doença geralmente ocorreu nos meses de dezembro, janeiro e fevereiro, 
-# porém nos anos de X e Y ocorrem em maior incidência nos meses de abril e maio).
+# ====================================================================
+# 1) Análise de frequências (1,0 ponto):
+# -  b) Em quais meses a doença_café foi mais incidente? Houve variação por ano? (ex: a doença geralmente ocorreu nos meses de dezembro, janeiro e fevereiro, porém nos anos de X e Y ocorrem em maior incidência nos meses de abril e maio).
+
+converte = function(x){
+  if(x < 20.1) return("baixo") 
+  else if(x > 60) return("alto") 
+  else return("medio") 
+}
+
+for(i in 1:nrow(df_total_alta))
+  df_total_alta$alta_categoria[i] = converte(df_total_alta$alta[i])
+
+for(i in 1:nrow(df_total_baixa))
+  df_total_baixa$alta_categoria[i] = converte(df_total_baixa$baixa[i])
+
+df_total_alta = df_total_alta[,c(27,1,2,28,3:26)]
+df_total_baixa = df_total_baixa[,c(27,1,2,28,3:26)]
+
+df_total_alta[,5:28] <- lapply(df_total_alta[,5:28],function(x){
+    options(digits=5)
+    as.double(sub(",", ".", x, fixed = TRUE))
+  }
+)
+
+df_total_baixa[,5:28] <- lapply(df_total_baixa[,5:28],function(x){
+    options(digits=5)
+    as.double(sub(",", ".", x, fixed = TRUE))
+  }
+)
+
+df_total_alta$P = apply(df_total_alta[,6:8], 1, mean)
+df_total_alta$UR = apply(df_total_alta[,9:11], 1, mean)
+df_total_alta$TMAX = apply(df_total_alta[,12:14], 1, mean)
+df_total_alta$TMIN = apply(df_total_alta[,15:17], 1, mean)
+df_total_alta$NDR.1mm = apply(df_total_alta[,18:20], 1, mean)
+df_total_alta$NDR.10mm = apply(df_total_alta[,21:23], 1, mean)
+df_total_alta$NRH80 = apply(df_total_alta[,24:26], 1, mean)
+df_total_alta$NRH90 = apply(df_total_alta[,27:29], 1, mean)
+
+df_total_baixa$P = apply(df_total_baixa[,6:8], 1, mean)
+df_total_baixa$UR = apply(df_total_baixa[,9:11], 1, mean)
+df_total_baixa$TMAX = apply(df_total_baixa[,12:14], 1, mean)
+df_total_baixa$TMIN = apply(df_total_baixa[,15:17], 1, mean)
+df_total_baixa$NDR.1mm = apply(df_total_baixa[,18:20], 1, mean)
+df_total_baixa$NDR.10mm = apply(df_total_baixa[,21:23], 1, mean)
+df_total_baixa$NRH80 = apply(df_total_baixa[,24:26], 1, mean)
+df_total_baixa$NRH90 = apply(df_total_baixa[,27:29], 1, mean)
+
+df_total_sA = df_total_alta[,c(1:5,29:36)]
+df_total_sB = df_total_baixa[,c(1:5,29:36)]
+
+mes_a<-c(0,0,0,0,0,0,0,0,0,0,0,0)
+for(i in 1:nrow(df_total_alta)){
+    if(df_total_alta$mes[i]=='janeiro'){
+      mes_a[1]=mes_a[1]+df_total_alta$alta[i]
+    }
+    else{
+      if(df_total_alta$mes[i]=='fevereiro'){
+        mes_a[2]=mes_a[2]+df_total_alta$alta[i]
+      }
+      else{
+        if(df_total_alta$mes[i]=='marco'){
+          mes_a[3]=mes_a[3]+df_total_alta$alta[i]
+        }
+        else{
+          if(df_total_alta$mes[i]=='abril'){
+            mes_a[4]=mes_a[4]+df_total_alta$alta[i]
+          }
+          else{
+            if(df_total_alta$mes[i]=='maio'){
+              mes_a[5]=mes_a[5]+df_total_alta$alta[i]
+            }
+            else{
+              if(df_total_alta$mes[i]=='junho'){
+                mes_a[6]=mes_a[6]+df_total_alta$alta[i]
+              }
+              else{
+                if(df_total_alta$mes[i]=='julho'){
+                  mes_a[7]=mes_a[7]+df_total_alta$alta[i]
+                }
+                else{
+                  if(df_total_alta$mes[i]=='agosto'){
+                    mes_a[8]=mes_a[8]+df_total_alta$alta[i]
+                  }
+                  else{
+                    if(df_total_alta$mes[i]=='setembro'){
+                      mes_a[9]=mes_a[9]+df_total_alta$alta[i]
+                    }
+                    else{
+                      if(df_total_alta$mes[i]=='outubro'){
+                        mes_a[10]=mes_a[10]+df_total_alta$alta[i]
+                      }
+                      else{
+                        if(df_total_alta$mes[i]=='novembro'){
+                          mes_a[11]=mes_a[11]+df_total_alta$alta[i]
+                        }
+                        else{
+                          if(df_total_alta$mes[i]=='dezembro'){
+                            mes_a[12]=mes_a[12]+df_total_alta$alta[i]
+                          }
+                        }}}}}}}}}}}
+ # }
+}
+meses = c('jan','fev','mar','abr','mai','jun','jul','ago','set','out','nov','dez')
+barplot(mes_a, col =  heat.colors(12), legend.text = meses, args.legend = list(x = "topright"))
+
+boxplot(
+  df_total_alta[which(df_total_alta$mes == 'janeiro'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'fevereiro'),]$alta, 
+  df_total_alta[which(df_total_alta$mes == 'marco'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'abril'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'maio'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'junho'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'julho'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'agosto'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'setembro'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'outubro'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'novembro'),]$alta,
+  df_total_alta[which(df_total_alta$mes == 'dezembro'),]$alta
+)
